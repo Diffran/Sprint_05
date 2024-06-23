769070918339ee8a93e0192f7b8d9202ce2b1489
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-23 16:57:53 +0200

    ?

---
+++ S05T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n02/S05T01N02FrancitorraDiana/controllers/FlowerController.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n02.S05T01N02FrancitorraDiana.controllers;public class FlowerController {
+}
---
+++ S05T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n02/S05T01N02FrancitorraDiana/model/domain/Flower.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n02.S05T01N02FrancitorraDiana.model.domain;public class Flower {
+}
