68b2f6534002a96792801d791b7eb32d30c77c2b
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-23 16:38:29 +0200

    first commit level 2

---
+++ S05T01N02FrancitorraDiana/flowers.sql
@@ -0,0 +1,17 @@
+
+-- -----------------------------------------------------
+-- Schema flowers
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `flowers` DEFAULT CHARACTER SET utf8 ;
+USE `flowers` ;
+
+-- -----------------------------------------------------
+-- Table `flowers`.`flower`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `flowers`.`flower` (
+  `pk_flowerid` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`pk_flowerid`))
+ENGINE = InnoDB;
+
