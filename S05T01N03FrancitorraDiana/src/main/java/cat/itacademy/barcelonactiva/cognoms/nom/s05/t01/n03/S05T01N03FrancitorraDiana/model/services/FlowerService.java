836276256451c5d2e1e5d86f9d27f275242b243c
Diffran <148889182+Diffran@users.noreply.github.com>
2024-06-27 13:51:40 +0200

    mig funciona falten coses

--- S05T01N03FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n03/S05T01N03FrancitorraDiana/model/services/FlowerService.java
+++ S05T01N03FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n03/S05T01N03FrancitorraDiana/model/services/FlowerService.java
@@ -1,4 +1,13 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n03.S05T01N03FrancitorraDiana.model.services;
 
+import cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n03.S05T01N03FrancitorraDiana.model.dto.FlowerDTO;
+
+import java.util.List;
+
 public interface FlowerService {
+    void create(FlowerDTO flowerDTO);
+    void update(FlowerDTO flowerDTO);
+    void delete(Integer flowerID);
+    FlowerDTO getOne(Integer flowerID);
+    List<FlowerDTO> getAll();
 }
--- S05T01N03FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n03/S05T01N03FrancitorraDiana/model/services/impl/ClientFlowerServiceImpl.java
+++ S05T01N03FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n03/S05T01N03FrancitorraDiana/model/services/impl/ClientFlowerServiceImpl.java
@@ -1,4 +1,46 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n03.S05T01N03FrancitorraDiana.model.services.impl;
 
-public class ClientFlowerServiceImpl {
+import cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n03.S05T01N03FrancitorraDiana.model.dto.FlowerDTO;
+import cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n03.S05T01N03FrancitorraDiana.model.services.FlowerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+public class ClientFlowerServiceImpl implements FlowerService {
+    @Autowired
+    private WebClient webClient;
+    public void create(FlowerDTO flowerDTO){
+
+    }
+    public void  update(FlowerDTO flowerDTO){
+
+    }
+    public void delete(Integer flowerID){
+
+    }
+    public FlowerDTO getOne(Integer flowerID){
+        FlowerDTO flowerDTO = webClient
+                .get()
+                .uri("/getOne/{id}")
+                .retrieve()
+                .bodyToMono(FlowerDTO.class)
+                .block();
+        return flowerDTO;
+    }
+    public List<FlowerDTO> getAll(){
+        Mono<List<FlowerDTO>> flowerListMono = webClient
+                .get()
+                .uri("/getAll")
+                .retrieve()
+                .bodyToFlux(FlowerDTO.class)
+                .collectList();
+
+        List<FlowerDTO> flowerList = flowerListMono.block();
+
+        return  flowerList;
+    }
 }
