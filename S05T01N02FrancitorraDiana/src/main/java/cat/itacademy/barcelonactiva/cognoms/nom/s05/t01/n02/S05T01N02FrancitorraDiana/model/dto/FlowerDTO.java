df1e29b436643e26cdcdb29d095ea3b64e8ffa5a
Diffran <148889182+Diffran@users.noreply.github.com>
2024-07-02 09:31:59 +0200

    arreglos

--- S05T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n02/S05T01N02FrancitorraDiana/model/dto/FlowerDTO.java
+++ S05T01N02FrancitorraDiana/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n02/S05T01N02FrancitorraDiana/model/dto/FlowerDTO.java
@@ -1,9 +1,7 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n02.S05T01N02FrancitorraDiana.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +15,7 @@ import java.util.List;
 public class FlowerDTO {
 
     private Integer pk_FlowerID;
+    @NonNull
     private String flowerName;
     private String flowerCountry;
     private String flowerType;
